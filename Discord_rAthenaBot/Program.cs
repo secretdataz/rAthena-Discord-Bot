2364d954a2bc3f595b47e25bae2ea0960d53e413
Jittapan Pluemsumran <foreverfk@stephack.com>
2017-05-08 10:18:28 +0700

    Now fully Mono-compatible

--- Discord_rAthenaBot/Program.cs
+++ Discord_rAthenaBot/Program.cs
@@ -4,25 +4,34 @@ using System.Diagnostics;
 
 namespace Discord_rAthenaBot
 {
-    partial class rAthenaBot
-    {
-        static void Main(string[] args)
-        {
-            String thisprocessname = Process.GetCurrentProcess().ProcessName;
+	partial class rAthenaBot
+	{
+		static void Main(string[] args)
+		{
+			// Mono code
+			if (Type.GetType("Mono.Runtime") != null)
+			{
+				// Just run it without any checks because Mono complains
+				new rAthenaBot();
+			}
+			else // Windows code
+			{
+				String thisprocessname = Process.GetCurrentProcess().ProcessName;
 
-            if (Process.GetProcesses().Count(p => p.ProcessName == thisprocessname) == 1)
-            {
-                rAthenaBot bot = new rAthenaBot();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Application already running. Please close the existing application.");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-                Environment.Exit(1);
-            }
+				if (Process.GetProcesses().Count(p => p.ProcessName == thisprocessname) == 1)
+				{
+					new rAthenaBot();
+				}
+				else
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("Application already running. Please close the existing application.");
+					Console.ForegroundColor = ConsoleColor.White;
+					Console.WriteLine("Press any key to exit...");
+					Console.ReadKey();
+					Environment.Exit(1);
+				}
+			}
         }
     }
 }
